4f1a30f3d7d51e12a1d07cc4bdcc16291f250114
Ashish Shah <ashishks@ashishks.pnq.csb>
2020-09-15 08:43:55 +0530

    log output of run-node-install.sh

--- tech-breakfix-ha-001/run-node-install.sh
+++ tech-breakfix-ha-001/run-node-install.sh
@@ -2,5 +2,5 @@
 date >> /tmp/log
 echo -e "running run-node-install-sh" >> /tmp/log
 cd /root/breakfix1
-ansible-playbook node-install.yml
+ansible-playbook node-install.yml >> /tmp/log 2>&1
 date >> /tmp/log
