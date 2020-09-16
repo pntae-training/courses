3916033bfe8f376ae72b0b3e7440d5d2a830c6bc
Ashish Shah <ashishks@ashishks.pnq.csb>
2020-09-16 14:43:38 +0530

    ansible to base migration of node-install in node-install-script.sh

--- tech-breakfix-ha-001/breakfix1/node-install-script.sh
+++ tech-breakfix-ha-001/breakfix1/node-install-script.sh
@@ -1,6 +1,6 @@
 for vm in nodea.example.com nodeb.example.com nodec.example.com
 do
-	ssh $vm	"sudo yum install pcs fence-agents-all lvm2-cluster gfs2-utils iscsi-initiator-utils httpd sysstat mariadb mariadb-server tcpdump wget sysstat  bzip2"
+	ssh $vm	"sudo yum -y install pcs fence-agents-all lvm2-cluster gfs2-utils iscsi-initiator-utils httpd sysstat mariadb mariadb-server tcpdump wget sysstat  bzip2"
 
 	ssh $vm "sudo wget https://raw.githubusercontent.com/pntae-training/courses/master/tech-breakfix-ha-001/breakfix1/mk -P /usr/local/bin/"
 	ssh $vm "sudo chmod 0755 /usr/local/bin/mk"
@@ -9,7 +9,7 @@ do
 
 	ssh $vm "sudo wget https://raw.githubusercontent.com/pntae-training/courses/master/tech-breakfix-ha-001/breakfix1/httpd.conf -P /etc/httpd/conf/"
 
-	ssh $vm "sudo echo redhat | passwd hacluster --stdin"
+	ssh $vm "sudo sh -c 'echo redhat | passwd hacluster --stdin'"
 
 	ssh $vm "sudo systemctl enable pcsd"
 	ssh $vm "sudo systemctl start pcsd"
@@ -44,17 +44,17 @@ ssh nodea.example.com "sudo pcs cluster setup --start --enable --name clusterhat
 
 ssh nodea.example.com "sudo pcs property set no-quorum-policy=freeze"
 
-ssh nodea.example.com "sudo pcs stonith create fence_node1 fence_ipmilan pcmk_host_list="nodea.example.com" ipaddr="192.168.47.220" login="admin" passwd="redhat" lanplus=1  power_wait=4; pcs stonith create fence_node2 fence_ipmilan pcmk_host_list="nodeb.example.com" ipaddr="192.168.47.221" login="admin" passwd="redhat" lanplus=1  power_wait=4; pcs stonith create fence_node3 fence_ipmilan pcmk_host_list="nodec.example.com" ipaddr="192.168.47.222" login="admin" passwd="redhat" lanplus=1  power_wait=4"
+ssh nodea.example.com "sudo sh -c 'pcs stonith create fence_node1 fence_ipmilan pcmk_host_list="nodea.example.com" ipaddr="192.168.47.220" login="admin" passwd="redhat" lanplus=1  power_wait=4; pcs stonith create fence_node2 fence_ipmilan pcmk_host_list="nodeb.example.com" ipaddr="192.168.47.221" login="admin" passwd="redhat" lanplus=1  power_wait=4; pcs stonith create fence_node3 fence_ipmilan pcmk_host_list="nodec.example.com" ipaddr="192.168.47.222" login="admin" passwd="redhat" lanplus=1  power_wait=4'"
 
-ssh nodea.example.com "sudo pcs constraint location fence_node1 prefers nodea.example.com; pcs constraint location fence_node2 prefers nodeb.example.com; pcs constraint location fence_node3 prefers nodec.example.com"
+ssh nodea.example.com "sudo sh -c 'pcs constraint location fence_node1 prefers nodea.example.com; pcs constraint location fence_node2 prefers nodeb.example.com; pcs constraint location fence_node3 prefers nodec.example.com'"
 
 ssh nodea.example.com "sudo pcs resource create dlm ocf:pacemaker:controld op monitor interval=30s on-fail=fence clone interleave=true ordered=true"
 
-ssh nodea.example.com "sudo /sbin/lvmconf --enable-cluster; systemctl disable lvm2-lvmetad --now"
+ssh nodea.example.com "sudo sh -c '/sbin/lvmconf --enable-cluster; systemctl disable lvm2-lvmetad --now'"
 
-ssh nodeb.example.com "sudo /sbin/lvmconf --enable-cluster; systemctl disable lvm2-lvmetad --now"
+ssh nodeb.example.com "sudo sh -c '/sbin/lvmconf --enable-cluster; systemctl disable lvm2-lvmetad --now'"
 
-ssh nodec.example.com "sudo /sbin/lvmconf --enable-cluster; systemctl disable lvm2-lvmetad --now"
+ssh nodec.example.com "sudo sh -c '/sbin/lvmconf --enable-cluster; systemctl disable lvm2-lvmetad --now'"
 
 ssh nodea.example.com "sudo pcs resource create clvmd ocf:heartbeat:clvm op monitor interval=30s on-fail=fence clone interleave=true ordered=true"
 
@@ -66,11 +66,11 @@ ssh nodea.example.com "sudo wipefs --all --force /dev/sda"
 
 ssh nodea.example.com "sudo wipefs --all --force /dev/sdb"
 
-ssh nodea.example.com "sudo pvcreate /dev/sda -ff -y; sleep 5"
+ssh nodea.example.com "sudo sh -c 'pvcreate /dev/sda -ff -y; sleep 5'"
 
-ssh nodea.example.com "sudo vgcreate -Ay -cy --shared webfs_vg /dev/sda; sleep 10"
+ssh nodea.example.com "sudo sh -c 'vgcreate -Ay -cy --shared webfs_vg /dev/sda; sleep 10'"
   
-ssh nodea.example.com "sudo lvcreate -L1.3G -n webfs_lv webfs_vg -y --config 'global { locking_type = 0 }'"
+ssh nodea.example.com "sudo sh -c 'lvcreate -L1.3G -n webfs_lv webfs_vg -y --config 'global { locking_type = 0 }''"
 
 
 for vm in nodea.example.com nodeb.example.com nodec.example.com
@@ -88,7 +88,7 @@ ssh nodea.example.com "sudo pcs constraint order start clvmd-clone then clusterf
 
 ssh nodea.example.com "sudo pcs resource create apache_res apache configfile="/etc/httpd/conf/httpd.conf" statusurl="http://127.0.0.1/server-status" --group web"
 
-ssh nodea.example.com "sudo pcs constraint order start clusterfs-clone then web;  pcs constraint location web prefers nodea.example.com"
+ssh nodea.example.com "sudo sh -c 'pcs constraint order start clusterfs-clone then web;  pcs constraint location web prefers nodea.example.com'"
 
 for vm in nodea.example.com nodeb.example.com nodec.example.com
 do
